f3c20e38c721a1477e070b45bb64afec19402906
Nils Quetschlich <nils.quetschlich@tum.de>
2025-05-16 13:20:19 +0200

    💡 removed duplicate file header

--- src/mqt/bench/devices/ionq.py
+++ src/mqt/bench/devices/ionq.py
@@ -7,13 +7,7 @@
 # Licensed under the MIT License
 
 """File to create a target device from the IonQ calibration data."""
-# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
-# Copyright (c) 2025 Munich Quantum Software Company GmbH
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
+
 
 from __future__ import annotations
 
--- src/mqt/bench/devices/quantinuum.py
+++ src/mqt/bench/devices/quantinuum.py
@@ -7,13 +7,6 @@
 # Licensed under the MIT License
 
 """File to create a target device from the Quantinuum calibration data."""
-# Copyright (c) 2023 - 2025 Chair for Design Automation, TUM
-# Copyright (c) 2025 Munich Quantum Software Company GmbH
-# All rights reserved.
-#
-# SPDX-License-Identifier: MIT
-#
-# Licensed under the MIT License
 
 from __future__ import annotations
 
