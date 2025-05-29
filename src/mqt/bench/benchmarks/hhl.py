6b6e92f7be0bbf4fb51e8f3aabd5929dcbf6d573
Simon <simon.t.hofmann@github.com>
2025-05-29 11:08:46 +0200

    :art: register hhl

--- src/mqt/bench/benchmarks/hhl.py
+++ src/mqt/bench/benchmarks/hhl.py
@@ -14,7 +14,10 @@ import numpy as np
 from qiskit import ClassicalRegister, QuantumCircuit, QuantumRegister
 from qiskit.circuit.library import QFTGate
 
+from ._registry import register
 
+
+@register("hhl")
 def create_circuit(num_qubits: int) -> QuantumCircuit:
     """HHL algorithm for a fixed 2x2 Hermitian matrix A using scalable QPE precision.
 
